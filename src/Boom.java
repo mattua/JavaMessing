45c4a8a9656ebe934c05087456116ec23065ce6c
Matt Thomas <Matt@Thomas.com>
2017-04-06 10:02:37 +0100

    boom

--- src/Boom.java
+++ src/Boom.java
@@ -3,12 +3,103 @@ import java.util.List;
 
 public class Boom {
 
+	public static class Byer extends Greeter {
+
+		public Byer(Bosh bosh, String name, Object lock) {
+			super(bosh, name, lock);	
+			
+			
+		}
+		
+		@Override
+		public void run() {
+			
+			try {
+				bosh.sayBye(name, lock);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	public static class Greeter implements Runnable {
+
+		protected Bosh bosh;
+		protected String name;
+		protected Object lock;
+		
+		public Greeter(Bosh bosh, String name,Object lock){
+			this.bosh=bosh;
+			this.name=name;
+			this.lock=lock;
+		}
+		
+		@Override
+		public void run() {
+			
+			try {
+				bosh.sayHello(name, lock);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
 		
 		System.out.println("boom");
 		
 		List<Customer> customers = new ArrayList<Customer>();
 		
+		Bosh bosh1 = new Bosh();
+		Bosh bosh2 = new Bosh();
+		
+		Integer lock1 = 1;
+		Integer lock2 = 2;
+		
+		// Key point here is if we use the same
+		// lock object and the same bosh instance
+		// then the calls will block
+		
+		// if we use the same monitor lock objec
+		// but on a different instance of bosh
+		// they WILL block - even if it is 
+		// a separate instance - the lock is on
+		// the monitor, not the object with the
+		// synchroised block
+		
+		// if we use the same bosh instance but
+		// different lock object, then the methods
+		// will NOT block
+		
+		
+		Greeter g1 = new Greeter(bosh1,"1",lock1);
+		Greeter g2 = new Greeter(bosh2,"2",lock1);
+		
+		
+		new Thread(new Greeter(bosh1,"1",lock1)).start();
+		new Thread(new Byer(bosh1,"2",lock1)).start();
+		
+		
+		
+		
+		
+		
+		
+		/*
 		while (true){
 			
 			Thread.sleep(2);
@@ -16,6 +107,22 @@ public class Boom {
 			customers.add(c1);
 			
 		}
+		*/
+		// wait and notify are used to
+		// avoid dead locks
+		
+		/*
+		 deadlock when all threads are waiting
+		 to enter a synchronised block but one
+		 thread is holding the lock and not going to
+		 leave
+		 
+		 
+		 
+		 */
+		
+		
+				
 		
 		
 	}
--- src/Bosh.java
+++ src/Bosh.java
@@ -1,4 +1,77 @@
 
 public class Bosh {
 
+	public void sayHello(String from,Object lock) throws Exception{
+		
+		synchronized (lock)
+		{
+			
+			
+			System.out.println("start Hello from "+from);
+			lock.wait();
+			Thread.sleep(10000);
+			System.out.println("end Hello from "+from);
+		}
+		
+	}
+	
+	// with no waits specified, the first
+	// method to acquire the lock will get it
+	// run to completion - before releasing the
+	// lock - so the second can begin the method
+	// so you will see 
+	/*
+	start Bye from 1
+	end Bye from 1
+	start Bye from 2
+	end Bye from 2
+	
+	REMEMBER WE CALL WAIT ON THE LOCK OBJECT
+	NOT THE OBJECT CONTAINING THE SYNCHRONISED
+	BLOCK (THOUGH THEY COULD BE THE SAME IF
+	SYNCHRONISING ON THIS
+	
+	if we put a wait Hello in one method after start,
+	but no notify in the bye method 
+	start Hello from 1
+	start Bye from 2
+	end Bye from 2
+	and the first one hangs....
+	
+	If we put a notifyAll in the second method
+	then WHEN that method completes and releases
+	the lock, the first thread is woken up and
+	will attempt to regain the lock. 
+	
+	Without the notify all, the first thread will
+	remain waiting forever, even though the second
+	one has release the locked having completed
+	
+	Notifyall does NOT let go of the lock. It
+	is a signal to other waiting threads to try to 
+	attempt to acquire the lock once the the notifyAll
+	thread releases the lock
+	
+	*/
+	
+	
+	
+	public void sayBye(String from,Object lock) throws Exception{
+		
+		synchronized (lock)
+		{
+			
+			System.out.println("start Bye from "+from);
+			lock.notifyAll();
+			Thread.sleep(10000);
+			
+			
+			System.out.println("end Bye from "+from);
+		
+			
+		
+		}
+		
+	}
+	
 }
