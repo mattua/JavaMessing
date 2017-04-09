f326f982902d8f5dcbf57ea3358acf3a962fa658
Matt Thomas <Matt@Thomas.com>
2017-04-09 14:30:37 +0100

    boom

--- src/ThreadingExamples.java
+++ src/ThreadingExamples.java
@@ -5,8 +5,92 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+
 public class ThreadingExamples {
 
+public static void main(String[] args) throws Exception {
+		
+		
+	
+	
+	
+		testJoin();
+		System.exit(1);
+		
+		//testInterrupted();
+		
+		System.out.println("boom town");
+		
+		List<Customer> customers = new ArrayList<Customer>();
+		
+		Bosh bosh1 = new Bosh();
+		Bosh bosh2 = new Bosh();
+		
+		Integer lock1 = 1;
+		Integer lock2 = 2;
+		
+		// Key point here is if we use the same
+		// lock object and the same bosh instance
+		// then the calls will block
+		
+		// if we use the same monitor lock objec
+		// but on a different instance of bosh
+		// they WILL block - even if it is 
+		// a separate instance - the lock is on
+		// the monitor, not the object with the
+		// synchroised block
+		
+		// if we use the same bosh instance but
+		// different lock object, then the methods
+		// will NOT block
+		
+		
+		Greeter g1 = new Greeter(bosh1,"1",lock1);
+		Greeter g2 = new Greeter(bosh2,"2",lock1);
+		
+		
+		new Thread(new Greeter(bosh1,"1",lock1)).start();
+		new Thread(new Byer(bosh1,"2",lock1)).start();
+		
+		
+		
+		
+		
+		
+		
+		/*
+		while (true){
+			
+			if (Math.random()<0.3){
+				Thread.sleep(1);
+			}
+			
+			Customer c1= new Customer();
+			customers.add(c1);
+			
+		}
+		*/
+		// wait and notify are used to
+		// avoid dead locks
+		
+		/*
+		 deadlock when all threads are waiting
+		 to enter a synchronised block but one
+		 thread is holding the lock and not going to
+		 leave
+		 
+		 
+		 
+		 */
+		
+		
+
+		
+		
+	}
+	
+	
+	
 	public static class Byer extends Greeter {
 
 		public Byer(Bosh bosh, String name, Object lock) {
@@ -169,82 +253,6 @@ public static void testJoin() throws InterruptedException{
 		
 	}
 	
-	public static void main(String[] args) throws Exception {
-		
-		testJoin();
-		System.exit(1);
-		
-		//testInterrupted();
-		
-		System.out.println("boom town");
-		
-		List<Customer> customers = new ArrayList<Customer>();
-		
-		Bosh bosh1 = new Bosh();
-		Bosh bosh2 = new Bosh();
-		
-		Integer lock1 = 1;
-		Integer lock2 = 2;
-		
-		// Key point here is if we use the same
-		// lock object and the same bosh instance
-		// then the calls will block
-		
-		// if we use the same monitor lock objec
-		// but on a different instance of bosh
-		// they WILL block - even if it is 
-		// a separate instance - the lock is on
-		// the monitor, not the object with the
-		// synchroised block
-		
-		// if we use the same bosh instance but
-		// different lock object, then the methods
-		// will NOT block
-		
-		
-		Greeter g1 = new Greeter(bosh1,"1",lock1);
-		Greeter g2 = new Greeter(bosh2,"2",lock1);
-		
-		
-		new Thread(new Greeter(bosh1,"1",lock1)).start();
-		new Thread(new Byer(bosh1,"2",lock1)).start();
-		
-		
-		
-		
-		
-		
-		
-		/*
-		while (true){
-			
-			if (Math.random()<0.3){
-				Thread.sleep(1);
-			}
-			
-			Customer c1= new Customer();
-			customers.add(c1);
-			
-		}
-		*/
-		// wait and notify are used to
-		// avoid dead locks
-		
-		/*
-		 deadlock when all threads are waiting
-		 to enter a synchronised block but one
-		 thread is holding the lock and not going to
-		 leave
-		 
-		 
-		 
-		 */
-		
-		
-
-		
-		
-	}
 	
 	
 	
