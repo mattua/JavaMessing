3a959b6e3451a7986a317b0b853f6772039c6478
Matt Thomas <Matt@Thomas.com>
2017-04-09 14:18:28 +0100

    rename

--- src/ThreadingExamples.java
+++ src/ThreadingExamples.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class Boom {
+public class ThreadingExamples {
 
 	public static class Byer extends Greeter {
 
